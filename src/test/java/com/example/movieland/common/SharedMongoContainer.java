226486dadf1ee99ef83de7a781872282d800cc46
YamiHeike <koscielniak.z@gmail.com>
2025-11-16 22:45:15 +0100

    fix: test container should close after the tests end

--- src/test/java/com/example/movieland/common/SharedMongoContainer.java
+++ src/test/java/com/example/movieland/common/SharedMongoContainer.java
@@ -4,8 +4,7 @@ import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.utility.DockerImageName;
 
 public class SharedMongoContainer {
-    public static final MongoDBContainer INSTANCE = new MongoDBContainer(DockerImageName.parse("mongo:4.4.18"))
-            .withReuse(true);
+    public static final MongoDBContainer INSTANCE = new MongoDBContainer(DockerImageName.parse("mongo:4.4.18"));
     static {
         INSTANCE.start();
     }
