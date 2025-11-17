187c1b4b71d9cf8c673ab36dd734be1d662557f6
YamiHeike <koscielniak.z@gmail.com>
2025-11-17 20:04:56 +0100

    chore: remove redundant test

--- src/test/java/com/example/movieland/actor/ActorServiceTest.java
+++ src/test/java/com/example/movieland/actor/ActorServiceTest.java
@@ -3,15 +3,11 @@ package com.example.movieland.actor;
 import com.example.movieland.common.BaseIntegrationTest;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
@@ -102,20 +98,6 @@ public class ActorServiceTest extends BaseIntegrationTest {
         var lastName = "Hanks";
         // when-then
         assertThatThrownBy(() -> actorService.findByFirstNameAndLastName(firstName, lastName)).isExactlyInstanceOf(ActorNotFound.class);
-    }
-
-    @ParameterizedTest
-    @MethodSource("invalidParams")
-    void throwException_whenNull(String firstName, String lastName) {
-        assertThatThrownBy(() -> actorService.findByFirstNameAndLastName(firstName, lastName)).isExactlyInstanceOf(NullPointerException.class);
-    }
-
-    static Stream<Arguments> invalidParams() {
-        return Stream.of(
-                Arguments.of("Leonardo", null),
-                Arguments.of(null, "DiCaprio"),
-                Arguments.of(null, null)
-        );
     }
 
     @Test
